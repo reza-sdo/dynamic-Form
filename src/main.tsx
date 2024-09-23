29edd517b789275eb8aed7e7a4ab8c0bed40d1b5
REZA <rezammdzd0@gmail.com>
2024-09-23 18:56:01 +0330

    done!

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
